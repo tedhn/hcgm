d1d11e4bf7e823167788f7a8d9577f5844b8c25e
Wallace <chandriowallace@gmail.com>
2025-04-10 21:09:51 +0700

    update 10/04/2025

--- prisma/migrations/20250409113250_updated_product_stock_type_to_int/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `stock` on the `Product` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Integer`.
-
-*/
--- AlterTable
-ALTER TABLE "Product" ALTER COLUMN "stock" SET DEFAULT 0,
-ALTER COLUMN "stock" SET DATA TYPE INTEGER;
