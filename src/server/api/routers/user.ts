658ae0f098e644e3908c46ea7efd43ab91df66ac
Ted <heinhtetnaing186@gmail.com>
2025-06-18 13:37:09 +0800

    f

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -224,6 +224,10 @@ export const userRouter = createTRPCRouter({
             });
           }
 
+          await ctx.db.transaction.deleteMany({
+            where: { ADMIN_ID: input.id },
+          });
+
           await ctx.db.admin.delete({
             where: { ID: input.id },
           });
@@ -233,6 +237,11 @@ export const userRouter = createTRPCRouter({
           };
         case "customer":
           // Delete the customer
+
+          await ctx.db.transaction.deleteMany({
+            where: { CUSTOMER_ID: input.id },
+          });
+
           await ctx.db.customer.delete({
             where: { ID: input.id },
           });
@@ -252,9 +261,12 @@ export const userRouter = createTRPCRouter({
     .input(createCustomerSchema)
     .mutation(async ({ input, ctx }) => {
       const existing = await ctx.db.customer.findFirst({
-        where: {
-          EMAIL: input.EMAIL,
-        },
+        where:
+          input.EMAIL !== ""
+            ? {
+                EMAIL: input.EMAIL,
+              }
+            : undefined,
       });
 
       if (existing) {
